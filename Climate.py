7c654d759460784f2b4240e1ceacf238c3b0fd11
kris vandendriessche <kris3140@gmail.com>
2022-04-17 08:03:54 +0200

    scraping

---
+++ Climate.py
@@ -0,0 +1,106 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+country = "india"
+city = "mumbai"
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'DNT': '1',  # Do Not Track Request Header
+    'Connection': 'close'
+}
+
+response = requests.get('https://www.climatestotravel.com/climate/' + country +'/' + city, headers=headers, timeout=5)
+
+try:
+    response.raise_for_status()
+except Exception as exc:
+    print('there was a problem: %s' % (exc))
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+print("temperature: ")
+temp_table = soup.find('tr', "min-table").parent
+for month in temp_table.find_all('th', scope="row"):
+    print(month.get_text())
+    for temp in month.parent.find_all('td', limit=3):
+        print(temp.get_text())
+
+print(); print()
+print("precipitation: ")
+precipit_table = soup.find('tr', "precipit-table").parent
+for month in precipit_table.find_all('th', scope="row"):
+    print(month.get_text())
+    for precipit in month.parent.find_all('td'):
+        print(precipit.get_text())
+
+# best_months = soup.find('span', id="best_time").parent.next_sibling.next_sibling.next_sibling
+best_month = ''
+x = 0
+for sibling in soup.find('span', id="best_time").parent.next_siblings:
+    x += 1
+    text = str(sibling)
+    if len(text) > 5:                  # ignore spam such as '\n' , <br>,... and only keep the long text
+        best_month += text
+    if x == 10: break
+best_month = best_month.replace('<strong>', '')
+best_month = best_month.replace('</strong>', '')
+best_month = best_month.replace('\n', ' ')
+print(); print("Best Month: \n", best_month)
+
+
+
+
+
+#
+# month =
+# <th scope="row" title="January">January</th>
+# <th scope="row" title="February">February</th>
+# <th scope="row" title="March">March</th>
+
+# print(temp_table.get_text())
+# temperatures = temp_table.find_all('td')
+# print(temperatures)
+# caption = temp_table.caption
+# print(caption.text)
+#
+
+# result = []
+# for child in soup.find('tr', "min-table").parent.next_siblings:
+#     if child != '\n':
+#         result.append(child.string)
+#
+# print(result)
+
+
+# .parent gives you the 'find' and everything below
+# tables = soup.find('tr', "min-table").parent
+# <table class="cities"><caption>Mumbai - Average temperatures (1991-2020) </caption><colgroup><col/><col class="celsius" span="3"/><col class="fahrenheit" span="3"/></colgroup><tr class="title-table-new"><th scope="col">Month</th><th scope="col" title="Average low temperatures (degrees Celsius)">Min (°C)</th><th scope="col" title="Average high temperatures (degrees Celsius)">Max (°C)</th><th scope="col" title="Average daily temperature (degrees Celsius)">Mean (°C)</th><th scope="col" title="Average low temperatures (degrees Fahrenheit)">Min (°F)</th><th scope="col" title="Average high temperatures (degrees Fahrenheit)">Max (°F)</th><th scope="col" title="Average daily temperature (degrees Fahrenheit)">Mean (°F)</th></tr><tr class="min-table"><th scope="row" title="January">January</th><td>17</td><td>31</td><td>24</td><td>62</td><td>88</td><td>75.3</td></tr><tr class="min-table"><th scope="row" title="February">February</th><td>18</td><td>32</td><td>25</td><td>65</td><td>89</td><td>77</td></tr><tr class="min-table"><th scope="row" title="March">March</th><td>21</td><td>33</td><td>27.2</td><td>70</td><td>92</td><td>80.9</td></tr><tr class="min-table"><th scope="row" title="April">April</th><td>24</td><td>33</td><td>28.8</td><td>75</td><td>92</td><td>83.9</td></tr><tr class="min-table"><th scope="row" title="May">May</th><td>27</td><td>34</td><td>30.2</td><td>80</td><td>93</td><td>86.4</td></tr><tr class="min-table"><th scope="row" title="June">June</th><td>26</td><td>32</td><td>29.4</td><td>79</td><td>90</td><td>85</td></tr><tr class="min-table"><th scope="row" title="July">July</th><td>25</td><td>30</td><td>27.9</td><td>78</td><td>87</td><td>82.2</td></tr><tr class="min-table"><th scope="row" title="August">August</th><td>25</td><td>30</td><td>27.8</td><td>77</td><td>87</td><td>82</td></tr><tr class="min-table"><th scope="row" title="September">September</th><td>25</td><td>31</td><td>28</td><td>77</td><td>88</td><td>82.3</td></tr><tr class="min-table"><th scope="row" title="October">October</th><td>24</td><td>34</td><td>28.8</td><td>75</td><td>93</td><td>83.8</td></tr><tr class="min-table"><th scope="row" title="November">November</th><td>21</td><td>34</td><td>27.8</td><td>71</td><td>93</td><td>82</td></tr><tr class="min-table"><th scope="row" title="December">December</th><td>18</td><td>33</td><td>25.5</td><td>65</td><td>91</td><td>77.8</td></tr><tr class="min-table"><th scope="row" title="Year">Year</th><td>22.7</td><td>32.4</td><td>27.5</td><td>72.9</td><td>90.3</td><td>81.5</td></tr></table>
+# extract
+# Fahrenheit)">Mean (°F)</th></tr><tr class="min-table"><th scope="row" title="January">January</th><td>17</td><td>31</td><td>24</td><td>62</td><td>88</td><td>75.3</td></tr><tr class="min-table"><th scope="row" title="February">February</th><td>18</td><td>32</
+
+# sunshine_table = soup.find('table', class_="sole")
+# print(sunshine_table)
+# sunshine table : only up to january:
+# <div class="div-tabella"><table class="sole"><caption>Mumbai - Sunshine hours</caption><tr class="title-table-new"><th scope="col">Month</th><th scope="col" title="Average daily sunshine hours">Average</th><th scope="col">Total</th></tr><th scope="row" title="January">January</th><td>9</td><td>280</td></table></div>
+
+# extract from 'soup' : after January there is a lot of bullshit, which makes it impossible to capture the full sole table
+# therefore we capture sunshine from an other website
+# <div class="div-tabella"><table class="sole"><caption>Mumbai - Sunshine hours</caption><tr class="title-table-new"><th scope="col">Month</th><th scope="col" title="Average daily sunshine hours">Average</th><th scope="col">Total</th></tr>
+# <th scope="row" title="January">January</th><td>9</td><td>280</td></table></div></div></div></div></form></body></html>
+# <th scope="row" title="February">February</th><td>9.5</td><td>275</td>
+# <th scope="row" title="March">March</th><td>9</td><td>280</td>
+# <th scope="row" title="April">April</th><td>9.5</td><td>280</td>
+# <th scope="row" title="May">May</th><td>9</td><td>275</td>
+# <th scope="row" title="June">June</th><td>4.5</td><td>140</td>
+# <th scope="row" title="July">July</th><td>2.5</td><td>80</td>
+# <th scope="row" title="August">August</th><td>2.5</td><td>80</td>
+# <th scope="row" title="September">September</th><td>5</td><td>145</td>
+# <th scope="row" title="October">October</th><td>7.5</td><td>240</td>
+# <th scope="row" title="November">November</th><td>9</td><td>265</td>
+# <th scope="row" title="December">December</th><td>9</td><td>275</td>
+# <th scope="row" title="Year">Year</th><td>7.2</td><td>2615</td>
+# <br/>
---
+++ Sunshine.py
@@ -0,0 +1,66 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+country = "india"
+city = "mumbai"
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'DNT': '1',  # Do Not Track Request Header
+    'Connection': 'close'
+}
+
+response = requests.get('https://www.weather-atlas.com/en/' + country + '/' + city + '-climate', headers=headers, timeout=5)
+
+try:
+    response.raise_for_status()
+except Exception as exc:
+    print('there was a problem: %s' % (exc))
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+
+print("Average sunshine: ")
+tabel = soup.find("a", text="Average sunshine in January").parent.parent
+print(tabel.find('li').span.get_text()[:-1])
+for line in tabel.find("li").next_siblings:
+    print(line.span.get_text()[:-1])
+
+tabel2 = soup.find("a", text="Average sunshine in July").parent.parent
+print(tabel2.find('li').span.get_text()[:-1])
+for line in tabel2.find("li").next_siblings:
+    print(line.span.get_text()[:-1])
+
+print("Average humidity: ")
+tabel = soup.find("a", text="Average humidity in January").parent.parent
+print(tabel.find('li').span.get_text()[:-1])
+for line in tabel.find("li").next_siblings:
+    print(line.span.get_text()[:-1])
+
+tabel2 = soup.find("a", text="Average humidity in July").parent.parent
+print(tabel2.find('li').span.get_text()[:-1])
+for line in tabel2.find("li").next_siblings:
+    print(line.span.get_text()[:-1])
+
+
+
+
+
+# tabel = soup.find("a", text="Average sunshine in January").parent.parent
+# tabel2 = soup.find("a", text="Average sunshine in July").parent.parent
+# <ul class="list-unstyled mb-0"><li><a href="/en/india/mumbai-weather-january#daylight_sunshine" title="Climate data
+# - January">Average sunshine in January</a>: <br class="br-xs"/><span class="fw-bold">8.7h
+# </span></li><li><a href="/en/india/mumbai-weather-february#daylight_sunshine" title="Climate data
+# - February">Average sunshine in February</a>: <br class="br-xs"/><span class="fw-bold">9.2
+# </span></li><li><a href="/en/india/mumbai-weather-march#daylight_sunshine" title="Climate data
+# - March">Average sunshine in March</a>: <br class="br-xs"/><span class="fw-bold">8.8h</span></li><li><a href="/en/india/mumbai-weather-april#daylight_sunshine" title="Climate data - April">Average sunshine in April</a>: <br class="br-xs"/><span class="fw-bold">9.5h</span></li><li><a href="/en/india/mumbai-weather-may#daylight_sunshine" title="Climate data - May">Average sunshine in May</a>: <br class="br-xs"/><span class="fw-bold">9.6h</span></li><li><a href="/en/india/mumbai-weather-june#daylight_sunshine" title="Climate data - June">Average sunshine in June</a>: <br class="br-xs"/><span class="fw-bold">5h</span></li></ul>
+
+# <ul class="list-unstyled mb-0"><li><a href="/en/india/mumbai-weather-july#daylight_sunshine" title="Climate data - July">Average sunshine in July</a>: <br class="br-xs"/><span class="fw-bold">2.4h</span></li><li><a href="/en/india/mumbai-weather-august#daylight_sunshine" title="Climate data - August">Average sunshine in August</a>: <br class="br-xs"/><span class="fw-bold">2.4h</span></li><li><a href="/en/india/mumbai-weather-september#daylight_sunshine" title="Climate data - September">Average sunshine in September</a>: <br class="br-xs"/><span class="fw-bold">5.5h</span></li><li><a href="/en/india/mumbai-weather-october#daylight_sunshine" title="Climate data - October">Average sunshine in October</a>: <br class="br-xs"/><span class="fw-bold">7.7h</span></li><li><a href="/en/india/mumbai-weather-november#daylight_sunshine" title="Climate data - November">Average sunshine in November</a>: <br class="br-xs"/><span class="fw-bold">8.2h</span></li><li><a href="/en/india/mumbai-weather-december#daylight_sunshine" title="Climate data - December">Average sunshine in December</a>: <br class="br-xs"/><span class="fw-bold">8.2h</span></li></ul>
+
+# one line = from <li> to </li>
+#  <li><a href="/en/india/mumbai-weather-april#daylight_sunshine" title="Climate data - April">Average sunshine in April</a>
+#  : <br class="br-xs"/><span class="fw-bold">9.5h</span></li>
+
