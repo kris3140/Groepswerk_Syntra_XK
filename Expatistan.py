0014148b441cd50dbc591090584375417919295d
kris vandendriessche <kris3140@gmail.com>
2022-05-02 19:22:16 +0200

    updates 02/05

--- Expatistan.py
+++ Expatistan.py
@@ -5,8 +5,8 @@ import re
 country = 'japan'
 city = "tokyo"
 
-# country = 'china'
-# city = "shanghai"
+country = 'china'
+city = "shanghai"
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
