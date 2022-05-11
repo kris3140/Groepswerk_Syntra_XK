65da501961919fd57808ba5908eb76d31ce37cc1
kris vandendriessche <kris3140@gmail.com>
2022-05-11 11:42:49 +0200

    updates 11/05

--- Pollution.py
+++ Pollution.py
@@ -2,7 +2,7 @@ from bs4 import Tag, NavigableString, BeautifulSoup
 import requests
 
 country = 'Belgium'
-city = "Brussels"
+city = "city2_values"
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
